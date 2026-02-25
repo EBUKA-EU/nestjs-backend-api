5c7ff6fff0557174726647ecf6834cd8f1599952
EBUKA EMMANUEL UGWU <ugwuchukwuebuka999@outlook.com>
2026-02-25 18:39:51 -0400

    Update main.ts to include deployed site in CORS

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       "http://localhost:5173",  // Vite dev server
-      "http://127.0.0.1:5173"
+      "http://127.0.0.1:5173",
+      "https://langlocalhub.netlify.app"
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
