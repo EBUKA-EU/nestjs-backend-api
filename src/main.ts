b8261b742709bd4ddecdf41aac06c11499021be3
EBUKA EMMANUEL UGWU <ugwuchukwuebuka999@outlook.com>
2026-02-27 01:04:14 -0400

    Add new CORS origin for interpreterhub

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,8 @@ async function bootstrap() {
     origin: [
       "http://localhost:5173",  // Vite dev server
       "http://127.0.0.1:5173",
-      "https://langlocalhub.netlify.app"
+      "https://langlocalhub.netlify.app",
+      "https://interpreterhub.netlify.app/"
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
